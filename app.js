a1417274d989cd455643b2b3efed344d9ecaefc5
illia GG <ilia.m.goncharov@gmail.com>
2019-02-08 17:45:03 +0300

    budget + percdntage in data structure, calc whole budget, calc percentage, update UI

--- app.js
+++ app.js
@@ -18,7 +18,6 @@
 
 // Module for data structure, add/remove operations constructor and public method of data manipulation
 var budgetController = (function() {
-
 	// Constructors for income/outcome data packages (names for paramets are different to avoid confusion)
 	var Expences = function(id, description, value) {
 		this.id = id;
@@ -30,7 +29,13 @@ var budgetController = (function() {
 		this.description  = description;
 		this.value = value;
 	};
-
+	var calcTotal = function(type) {
+		var sum = 0;
+		data.allItems[type].forEach(function(curent) {
+			sum += curent.value;
+		});	
+		data.totals[type] = sum;
+	};
 	// Data structure 
 	var data = {
 		allItems: {
@@ -40,21 +45,21 @@ var budgetController = (function() {
 		totals: {
 			exp: 0,
 			inc: 0
-		}
+		},
+		budget: 0,
+		// -1 means not exist 
+		percentage: -1 
 	};
-
 	// Public method to add package to data structure (names for paramets are different to avoid confusion)
  	return {
 		addItem: function(type, des, val) {
 			var newItem, ID;
-
 			if ( data.allItems[type].length > 0) {
 				// Create new ID 
 				ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
 			} else {
 				ID = 0;
 			};
-			
 			// Create new Item based on 'inc' and 'exp' type
 			if(type === 'exp') {
 				newItem = new Expences(type, des, val);
@@ -64,23 +69,36 @@ var budgetController = (function() {
 			// Push to data structure
 			data.allItems[type].push(newItem);
 			// TEST // data.allItems.type.push(newItem);
-
 			// Return New Element
 			return newItem;
 		},
+		calculateBudget: function() {
+			// calculate all income and expenses 
+			calcTotal('inc');
+			calcTotal('exp');
+			// calc budget (income - expenses)
+			data.budget = data.totals.inc - data.totals.exp; 
+			// calc percentage
+			data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100); 
+		},
+		// Return Budget 
+		gatBudget: function() {
+			return {
+				budget: data.budget,
+				expTotal: data.totals.exp,
+				incTotal: data.totals.inc,
+				percentage: data.percentage	
+			}
+		},
 
+		// Basic Testing public method 
 		testing: function() {
 			console.log(data);
 		}
-
 	};
-
 })();
 
-
-
 var UIControler = (function() {
-
 	var DOMstrings = {
 		inpuType: '.add__type',
 		inputDescription: '.add__description',
@@ -89,27 +107,23 @@ var UIControler = (function() {
 		incomeContainer: '.income__list',
 		expensesContainer: '.expenses__list'
 	};
-
 	return {
 		getInput: function() {
 			return {
 				type: document.querySelector(DOMstrings.inpuType).value, // inc or exp
 				description: document.querySelector(DOMstrings.inputDescription).value,
-				value: document.querySelector(DOMstrings.inpuValue).value,
+				value: parseFloat(document.querySelector(DOMstrings.inpuValue).value)
 			}
 		},
-
 		getDomStrings: function() {
 			return DOMstrings;
 		},
-
 		// Update UI
 		addListItem: function(obj, type) {
 			// Create HTML string with a placeholder text
 			var html,
 				newHTML,
 				element;
-
 			if (type === 'inc') {
 				element = DOMstrings.incomeContainer;
 				html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div><div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>';
@@ -117,37 +131,26 @@ var UIControler = (function() {
 				element = DOMstrings.expensesContainer;
 				html = '<div class="item clearfix" id="expense-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__percentage">21%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>';
 			} else {};
-
 			// Add real data to placeholder
 			// TEST // var newHTML = html.replace(searchValue: '%id%', replaceValue: 'obj.id').replace(searchValue: '%description%', replaceValue: 'obj.description');
 			newHTML = html.replace('%id%', obj.id);
 			newHTML = newHTML.replace('%description%', obj.description);
 			newHTML = newHTML.replace('%value%', obj.value);
-
 			// Add HTML to DOM
 			document.querySelector(element).insertAdjacentHTML('beforeend', newHTML);
-
 		}, 
-
 		clearFields: function() {
-
 			var fields, fieldsArr;
-
 			fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inpuValue);
-
-			console.log(fields);
-
+			// console.log(fields);
 			// Slice trick to make ann array of HTML list
 			fieldsArr = Array.prototype.slice.call(fields);
-
 			// forEach method is great to work with arrays (it can recieve up to 3 arguments/parametrs)
 			fieldsArr.forEach(function(current, index, array) {
 				current.value = "";
 			});
-
 			fieldsArr[0].focus();
-
-			console.log(fieldsArr);
+			// console.log(fieldsArr);
 		}
 	};
 
@@ -156,7 +159,6 @@ var UIControler = (function() {
 
 
 var controller = (function(budgCo, UICo) {
-	
 	function setEventListeners() {
 		var DOM = UICo.getDomStrings();
 		// console.log(DOM);
@@ -167,25 +169,29 @@ var controller = (function(budgCo, UICo) {
 			} else {};
 		});		
 	};
+	var updateBudget = function() {
+		// Calc budget 
+		budgetController.calculateBudget();
+		// Return budget 
+		var budgetArr = budgetController.gatBudget();
+		// Display budget UI
+		document.querySelector('.budget__value').textContent = budgetArr.budget;
+	};
 
 	var ctrlAddItem = function() {
 		// 1. Get input
 		var input = UICo.getInput();
-		// console.log(input);
-
-		// 2. Add data to budgetController 
-		newItem = budgetController.addItem(input.type, input.description, input.value);
-
-		// 3. Update UI
-		UICo.addListItem(newItem, input.type);
-
-		// 3.5 Clear Feilds 
-		UIControler.clearFields();
-
-		// 4. Calc Budget 
- 
-		// 5. Update Budget UI
-
+		console.log(input);
+		if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+			// 2. Add data to budgetController 
+			newItem = budgetController.addItem(input.type, input.description, input.value);
+			// 3. Update UI
+			UICo.addListItem(newItem, input.type);
+			// 4 Clear Feilds 
+			UIControler.clearFields();
+			// 5. Update Budget UI
+			updateBudget();
+		} 
 	};
 
 	return {
